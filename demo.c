bacbcf031fbced357b9e96d767563b79887d24c6
phzh <p.zhavoronkov@gmail.com>
2025-01-11 23:38:06 +0100

    3 print lines

--- demo.c
+++ demo.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
-int main() {
-   int age;
-   printf("Hello, John\n");
-   printf("Hi, Frank\n");
-   printf("Hi, Billy Bob\n");
 
-   printf("how old are you?\n");
-   scanf("%d" ,&age);
-   printf("age is %d", age);
+int main() {
+   printf("Welcome\n");
+   printf("Citizen\n");
+   printf("Kane!\n");
    return 0;
 }

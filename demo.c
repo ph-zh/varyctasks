288a28ef6597981dee4c598bd0a04b84aef944d1
phzh <p.zhavoronkov@gmail.com>
2025-01-11 20:26:41 +0100

    c demo

---
+++ demo.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+   int age;
+   // printf() displays the string inside quotation
+   printf("Hello, John\n");
+   printf("Hi, Frank\n");
+   printf("Hi, Billy Bob\n");
+
+   printf("how old are you?\n");
+   scanf("%d" ,&age);
+   printf("age is %d", age);
+   return 0;
+}

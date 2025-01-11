1813c9da18bc2c353efcf50d74555465e3c11fac
phzh <p.zhavoronkov@gmail.com>
2025-01-12 00:12:22 +0100

    new line, tab added

--- demo.c
+++ demo.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
 int main() {
-   printf("Welcome\n");
-   printf("Citizen\n");
-   printf("Kane!\n");
+   
+   /*this is a mltiline comment
+   which i would like to consider
+   in this comment
+   \n = new line
+   \t = tab
+   */
+   printf("1\t2\t3\n4\t5\t6\n7\t8\t9");
    return 0;
 }

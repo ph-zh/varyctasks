c5868e7276228fe7fbc5da40f414588addd51176
phzh <p.zhavoronkov@gmail.com>
2025-01-12 23:05:20 +0100

    user input task

--- demo.c
+++ demo.c
@@ -1,32 +1,23 @@
 #include <stdio.h>
-
+#include <string.h>
 
 int main() {
 
-    // augmented assignment operators = used to replace a statement where an operator
-    //                                  takes a variable as one of its arguments
-    //                                  and then assigns the results back to the same variable
-    //                                  x = x + 1;
-    //                                  x+=1;
+    char name[25]; // bytes
+    int age;
 
-    int x = 10;
-
-    // x = x + 2;
-    // x+=2;
-
-    // x = x - 3;
-    // x-=3;
-
-    // x = x * 4;
-    // x*=4;
-
-    // x = x / 5;
-    // x/=5;
-
-    x = x % 4;
-    // x%=2;
-
-    printf("%d\n", x);
+    printf("What your name?\n");
+    // scanf("%s", &name); // function which allows to get user input before space
+    fgets(name, 25, stdin); // function which allows to get user input with space
+    name[strlen(name)-1] = '\0';
+    
+    printf("Ah, Ok, so you are %s\n", name);
+    
+    printf("How old are you ?\n");
+    scanf("%d", &age);
+    printf("You are %d years old\n", age);
+    
+    printf("Nice to meet you %s who is %d\n", name, age);
 
 return 0;
 }

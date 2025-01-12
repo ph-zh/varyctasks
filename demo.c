3d4e48969f3c29f26a830e42b05497fff21892bd
phzh <p.zhavoronkov@gmail.com>
2025-01-12 14:06:21 +0100

    variable declaration and initiatlization

--- demo.c
+++ demo.c
@@ -2,12 +2,18 @@
 
 int main() {
    
-   /*this is a mltiline comment
-   which i would like to consider
-   in this comment
-   \n = new line
-   \t = tab
-   */
-   printf("1\t2\t3\n4\t5\t6\n7\t8\t9");
-   return 0;
+int x; // declaration
+x = 10;// initialization
+int y = 20; // declaration + initialization
+
+int age  = 15;
+float gpa = 2.05;//floating point number
+char currency = '$'; // single character
+char name[] = "Frank"; // array of charactes
+
+printf("Hello %s\n", name);
+printf("You are %.d years old\n", age);
+printf("You have %f cois in the pocket\n", gpa);
+printf("Your favorite currency is %c\n", currency);
+return 0;
 }

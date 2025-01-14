1a977e91a4facad0af01972242de968e992bf45e
phzh <p.zhavoronkov@gmail.com>
2025-01-14 23:40:40 +0100

    circle circumreference

--- demo.c
+++ demo.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
-#include <math.h>
 
 int main() {
 
-   double A = sqrt(9);
-   double B = pow(2, 4);
-   int C = round(3.14);
-   int D = ceil(3.14);
-   int E = floor(3.99);
-   double F = fabs(-100);
-   double G = log(3);
-   double H = sin(45);
-   double I = cos(45);
-   double J = tan(45);
+   const double PI = 3.14159;
+   double radius;
+   double circumreference;
+   double area;
 
-   printf("%lf\n", H);
+   printf("Enter radius of a circle: \n");
+   scanf("%lf", &radius);
+
+   circumreference = 2 * PI * radius;
+   area = PI * radius * radius;
+
+   printf("circumreference: %lf\n", circumreference);
+   printf("area: %lf\n", area);
 
 return 0;
 }

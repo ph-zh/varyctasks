9de3b48c9d6858b410ce2ff9c3160599b6a33229
phzh <p.zhavoronkov@gmail.com>
2025-01-16 17:33:14 +0100

    switch statement

--- demo.c
+++ demo.c
@@ -2,19 +2,44 @@
 
 int main() {
 
-   int age;
+   // switch = A more efficient alternative to using many "else if" statements
+   //          allows a value to be tested for equality against many cases
 
-   printf("\nEnter your age: ");
-   scanf("%d", &age);
+   char grade;
 
-   if(age >= 18) {
-      printf("You are now signed up!\n");
-   } else if (age < 0) {
-      printf("You have not been born yet!\n");
-   } else if (age == 0) {
-      printf("You cant sign up! You were just born!\n");
-   } else {
-      printf("You are too young to signed up!\n");
+   printf("\nEnter a letter grade: ");
+   scanf("%c", &grade);
+
+   // if(grade == 'A') {
+   //    printf("Perfect!\n");
+   // } else if (grade == 'B') {
+   //    printf("You did good\n");
+   // } else if (grade == 'C') {
+   //    printf("You did okay\n");
+   // } else if (grade == 'D') {
+   //    printf("At least its not an F\n");
+   // } else if (grade == 'F') {
+   //    printf("Thats not a valid grade\n");
+   // }
+
+   switch(grade) {
+      case 'A':
+      printf("Perfect!\n");
+      break;
+      case 'B':
+      printf("You did good\n");
+      break;
+      case 'C':
+      printf("You did okay\n");
+      break;
+      case 'D':
+      printf("At least its not an F\n");
+      break;
+      case 'F':
+      printf("Thats not a valid grade\n");
+      break;
+      default:
+      printf("Please enter only valid grades\n");
    }
 
 return 0;

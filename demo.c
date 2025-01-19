60220331aeeafb3d01fd11fb1f9b033a3dd7e84e
phzh <p.zhavoronkov@gmail.com>
2025-01-19 16:50:00 +0100

    return statement

--- demo.c
+++ demo.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
 
-void birthday();
+double square(double x) {
+   return x * x * x;
+}
+// return == returns a value back to a caling function
 
 int main() {
 
-   char name[] = "Franky Spignetty";
-   int age = 21;
-   birthday(name, age);
-
+   double x = square(3.14);
+   printf("%lf\n", x);
    return 0;
-}
-
-void birthday(char name[], int age) {
-   printf("Happy Birthday dear! %s\n", name);
-   printf("You are %d yeas old!\n", age);
 }

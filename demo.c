b296db0b858f351055929b6ab860fb00ed3a2f1e
phzh <p.zhavoronkov@gmail.com>
2025-01-19 16:16:48 +0100

    declaration and discription of the birthday function before and after main

--- demo.c
+++ demo.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
 
-// function cant see inside other function. Eg main and birthday
-// arguments inside current function names parameters
-// name of the parameters could be renamed unless they keep same type as char and int
-
-void birthday(char x[], int y) {
-   printf("Happy Birthday dear %s!\n", x);
-   printf("You are %d yeas old!\n", y);
-}
+void birthday();
 
 int main() {
 
    char name[] = "Franky Spignetty";
    int age = 21;
-   // passing name and age variables as arguments
    birthday(name, age);
 
    return 0;
+}
+
+void birthday(char name[], int age) {
+   printf("Happy Birthday dear! %s\n", name);
+   printf("You are %d yeas old!\n", age);
 }

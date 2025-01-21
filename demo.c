a7fced60700924b303647705e8d7aebcf3f8c605
phzh <p.zhavoronkov@gmail.com>
2025-01-21 23:38:00 +0100

    string functions

--- demo.c
+++ demo.c
@@ -1,31 +1,20 @@
-/*
- * Function prototype
- * What is it ?
- * Function declaration, w/o a body, before main()
- * Ensures that calls to a function are made with correct arguments
- *
- * IMPORTANT NOTES
- * Many C compilers do not check for the parameter matching
- * Missing arguments will result in unexpected behavior
- * A function prototype causes the compiler to flag an error if arguments are missing
- *
- * ADVANTAGES
- * 1. Easier to navigate a program w/ main() at the top
- * 2. Heps with debugging
- * 3. Commonly used in header files
- */
 #include <stdio.h>
-
-void hello(char name[], int age); // function prototype declaration
+#include <string.h>
+#include <ctype.h>
 
 int main() {
 
-    char name[] = "Billy";
-    int age = 60;
-    hello(name, age);
-    return 0;
-}
+    char string1[] = "Billy";
+    char string2[] = "Bob";
 
-void hello(char name[], int age) {
-    printf("Hello %s you are %d years old\n", name, age);
+    // strlwr(string1); // converts a string to lowercase
+    // strupr(string1); // coverts a string to uppercase
+    // strcat(string1, // string2); appends string 2 to end of string1
+    // strncan(string1, // string2, 1); appends n characters from string2 to string1
+    // strnpy(string1, // string2); copy string2 to string1
+    // strncpy(string1, // string2, 4); copy n characters of string2 to string1
+
+    printf("%s\n", string1);
+
+    return 0;
 }

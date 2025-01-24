f12513615f8ce657708689a2f44525c0ad09741a
phzh <p.zhavoronkov@gmail.com>
2025-01-24 20:14:08 +0100

    while loop

--- demo.c
+++ demo.c
@@ -1,12 +1,25 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
 
-    // for loop = repeats a section of code a limited amount of times
-    // 3 statements needed: declare index (i), condition (i >= 1), increment/decrement of the index)
+    // while loop = repeats a section of code possibly unlimited times
+    // WHILE some condition remains true
+    // a while loop might not execute at all
 
-    for(int i = 10; i >= 1; i-=3) {
-        printf("%d\n", i);
+    char name[25];
+    printf("What is your name?: \n");
+    fgets(name, 25, stdin);
+    name[strlen(name) -1] = '\0';
+
+    while(strlen(name) == 0) {
+        printf("You did not enter your name");
+        printf("What is your name?: \n");
+        fgets(name, 25, stdin);
+        name[strlen(name) -1] = '\0';
     }
+
+    printf("Hello %s", name);;
+
     return 0;
 }

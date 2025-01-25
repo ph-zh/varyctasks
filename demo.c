b0951b70a27164368f6cdd52a0255f5ee8e1e29d
phzh <p.zhavoronkov@gmail.com>
2025-01-25 14:57:21 +0100

    do while loop

--- demo.c
+++ demo.c
@@ -1,25 +1,22 @@
 #include <stdio.h>
-#include <string.h>
 
 int main() {
 
-    // while loop = repeats a section of code possibly unlimited times
-    // WHILE some condition remains true
-    // a while loop might not execute at all
+    // while loop = checks a condition, THEN executes a block of ode if condition is true
+    // do while loop = always executes a block of code once, THEN checks a condition
 
-    char name[25];
-    printf("What is your name?: \n");
-    fgets(name, 25, stdin);
-    name[strlen(name) -1] = '\0';
+    int number = 0;
+    int sum = 0;
 
-    while(strlen(name) == 0) {
-        printf("You did not enter your name");
-        printf("What is your name?: \n");
-        fgets(name, 25, stdin);
-        name[strlen(name) -1] = '\0';
-    }
+     do {
+        printf("Enter a number above 0: \n");
+        scanf("%d", &number);
+        if (number > 0) {
+            sum += number;
+        }
+    } while (number > 0);
 
-    printf("Hello %s", name);;
+    printf("sum: %d\n", sum);
 
     return 0;
 }

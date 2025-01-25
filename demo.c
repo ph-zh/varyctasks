9826b08011d96bbc22b20930da2421ac263ab0dd
phzh <p.zhavoronkov@gmail.com>
2025-01-25 18:47:21 +0100

    print array with loop

--- demo.c
+++ demo.c
@@ -2,17 +2,12 @@
 
 int main() {
 
-    // array = a data structure that can store many values of the same data type.
+    double prices[] = {5.0, 10.00, 15.0, 25.0, 20.0, 30.0};
+    // sizeof from array shows as 6 elements multyply on 8, which is size of 1 double element
+    // printf("%d bytes\n", sizeof(prices));
 
-    double prices[10] = {5.0, 10.00, 15.0, 25.0, 20.0};
-    // double prices[5];
-    // prices[0] = 5.0;
-    // prices[1] = 10.0;
-    // prices[2] = 15.0;
-    // prices[3] = 25.0;
-    // prices[4] = 20.0;
-
-    printf("$%.2lf\n", prices[5]);
-
+    for (int i = 0; i < sizeof(prices)/sizeof(prices[0]); i++) {
+        printf("$%.2lf\n", prices[i]);
+    }
     return 0;
 }

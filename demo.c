c804ccbfa539860b215601a2dbadd411fa401f0f
phzh <p.zhavoronkov@gmail.com>
2025-01-25 22:41:23 +0100

    2D array

--- demo.c
+++ demo.c
@@ -1,13 +1,38 @@
 #include <stdio.h>
 
 int main() {
+    // 2D array = an array, where each element is an entire array
+    //            useful if you need a matrix, grid or table of data
 
-    double prices[] = {5.0, 10.00, 15.0, 25.0, 20.0, 30.0};
-    // sizeof from array shows as 6 elements multyply on 8, which is size of 1 double element
-    // printf("%d bytes\n", sizeof(prices));
+    // int numbers[2][3] = {
+    //     {1,2,3},
+    //     {4,5,6}
+    // };
 
-    for (int i = 0; i < sizeof(prices)/sizeof(prices[0]); i++) {
-        printf("$%.2lf\n", prices[i]);
+    int numbers[3][3];
+
+    int rows = sizeof(numbers) / sizeof(numbers[0]);
+    int columns = sizeof(numbers[0]) / sizeof(numbers[0][0]);
+
+    printf("rows: %d\n", rows);
+    printf("columns: %d\n", columns);
+
+    numbers[0][0] = 1;
+    numbers[0][1] = 2;
+    numbers[0][2] = 3;
+    numbers[1][0] = 4;
+    numbers[1][1] = 5;
+    numbers[1][2] = 6;
+    numbers[2][0] = 7;
+    numbers[2][1] = 8;
+    numbers[2][2] = 9;
+
+    for (int i = 0; i < rows; i++) { // as row
+        for (int j = 0; j < columns; j++) { // as columns
+            printf("%d", numbers[i][j]);
+        }
+        printf("\n"); // new line after each row
     }
+
     return 0;
 }

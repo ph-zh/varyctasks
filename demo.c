8337312b805d48822c6f2ecd1cad2ea995316604
phzh <p.zhavoronkov@gmail.com>
2025-01-25 23:22:43 +0100

    array of strings

--- demo.c
+++ demo.c
@@ -1,38 +1,17 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-    // 2D array = an array, where each element is an entire array
-    //            useful if you need a matrix, grid or table of data
+    // array of strings
+    char cars[][10] = {"Mustang", "Corvette", "Camaro"};
+    int size = sizeof(cars) / sizeof(cars[0]);
+    printf("Number of cars in the pool: %d\n", size); // array size
 
-    // int numbers[2][3] = {
-    //     {1,2,3},
-    //     {4,5,6}
-    // };
-
-    int numbers[3][3];
-
-    int rows = sizeof(numbers) / sizeof(numbers[0]);
-    int columns = sizeof(numbers[0]) / sizeof(numbers[0][0]);
-
-    printf("rows: %d\n", rows);
-    printf("columns: %d\n", columns);
-
-    numbers[0][0] = 1;
-    numbers[0][1] = 2;
-    numbers[0][2] = 3;
-    numbers[1][0] = 4;
-    numbers[1][1] = 5;
-    numbers[1][2] = 6;
-    numbers[2][0] = 7;
-    numbers[2][1] = 8;
-    numbers[2][2] = 9;
-
-    for (int i = 0; i < rows; i++) { // as row
-        for (int j = 0; j < columns; j++) { // as columns
-            printf("%d", numbers[i][j]);
-        }
-        printf("\n"); // new line after each row
+    // cars[0] = "Tesla";
+    strcpy(cars[0], "Tesla");
+    for (int i = 0; i < sizeof(cars) / sizeof(cars[0]); i++) {
+        printf("%d. ", i + 1);
+        printf("%s\n", cars[i]);
     }
-
     return 0;
 }

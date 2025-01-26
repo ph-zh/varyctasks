05baccc1b33955c8f32159340463ed2132cc84bc
phzh <p.zhavoronkov@gmail.com>
2025-01-26 21:42:04 +0100

    sort an array

--- demo.c
+++ demo.c
@@ -1,30 +1,43 @@
 #include <stdio.h>
-#include <string.h>
+
+void sort(char array[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (array[j] > array[j+1]) {
+                int temp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = temp;
+            }
+        }
+    }
+}
+
+void printArray(char array[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%c ", array[i]);
+    }
+}
 
 int main() {
 
-    // Swap values of two variables
+    // sort an array
+    /*
+    *int: Usually 4 bytes (32 bits).
+    float: Usually 4 bytes (32 bits).
+    double: Usually 8 bytes (64 bits).
+    long double: Typically 10, 12, or 16 bytes, depending on the system.
+     */
 
-    // char x = 'X';
-    // char y = 'Y';
-    // char temp;
-
-    // temp = x;
-    // x = y;
-    // y = temp;
-
-    char x[15] = "Water";
-    char y[15] = "Soda";
-    char temp[15];
-
-    strcpy(temp, x);
-    strcpy(x, y);
-    strcpy(y, temp);
-
-    printf("x = %s\n", x);
-    printf("y = %s\n", y);
+    //int array[] = {9,1,8,2,7,3,6,4,5};
+    char array[] = {'F', 'A', 'D', 'B', 'C'}; // sorting by Char
+    //int size = sizeof(array); // size of array in bytes
+    // size of 1 int element is 4 bytes
+    int size = sizeof(array)/sizeof(array[0]); // size of array
+    // printf("%d\n", size);
 
 
+    sort(array, size);
+    printArray(array, size);
 
     return 0;
 }

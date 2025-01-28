d48c198a023d9c3be2f657cb66a8bd5182a03f1a
phzh <p.zhavoronkov@gmail.com>
2025-01-28 21:57:01 +0100

    struct

--- demo.c
+++ demo.c
@@ -1,42 +1,31 @@
 #include <stdio.h>
+#include <string.h>
 
-void sort(char array[], int size) {
-    for (int i = 0; i < size - 1; i++) {
-        for (int j = 0; j < size - i - 1; j++) {
-            if (array[j] < array[j+1]) {
-                int temp = array[j];
-                array[j] = array[j+1];
-                array[j+1] = temp;
-            }
-        }
-    }
-}
-
-void printArray(char array[], int size) {
-    for (int i = 0; i < size; i++) {
-        printf("%c ", array[i]);
-    }
-}
+struct Player {
+    char name[12];
+    int score;
+};
 
 int main() {
 
-    // sort an array
-    /*
-    *int: Usually 4 bytes (32 bits).
-    float: Usually 4 bytes (32 bits).
-    double: Usually 8 bytes (64 bits).
-    long double: Typically 10, 12, or 16 bytes, depending on the system.
-     */
+    // struct = collection of related members ("variables")
+    //          they can be of different data types
+    //          listed under one name in a block of memory
+    //          very similar to classes in other languages (but no methods)
 
-    char array[] = {'A','X','Z','E', 'C', 'F'};
-    //int size = sizeof(array); // size of array in bytes
-    // size of 1 int element is 4 bytes
-    int size = sizeof(array)/sizeof(array[0]); // size of array
-    // printf("%d\n", size);
+    struct Player player1;
+    struct Player player2;
 
+    strcpy(player1.name, "Frank");
+    player1.score = 4;
 
-    sort(array, size);
-    printArray(array, size);
+    strcpy(player2.name, "John");
+    player2.score = 5;
+
+    printf("%s\n", player1.name);
+    printf("%s\n", player2.name);
+    printf("%d\n", player1.score);
+    printf("%d\n", player2.score);
 
     return 0;
 }

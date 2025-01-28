33a25de47c08a006e30b4618e1c938599fee0b2e
phzh <p.zhavoronkov@gmail.com>
2025-01-28 21:34:58 +0100

    sort an array with chars

--- demo.c
+++ demo.c
@@ -3,7 +3,7 @@
 void sort(char array[], int size) {
     for (int i = 0; i < size - 1; i++) {
         for (int j = 0; j < size - i - 1; j++) {
-            if (array[j] > array[j+1]) {
+            if (array[j] < array[j+1]) {
                 int temp = array[j];
                 array[j] = array[j+1];
                 array[j+1] = temp;
@@ -28,8 +28,7 @@ int main() {
     long double: Typically 10, 12, or 16 bytes, depending on the system.
      */
 
-    //int array[] = {9,1,8,2,7,3,6,4,5};
-    char array[] = {'F', 'A', 'D', 'B', 'C'}; // sorting by Char
+    char array[] = {'A','X','Z','E', 'C', 'F'};
     //int size = sizeof(array); // size of array in bytes
     // size of 1 int element is 4 bytes
     int size = sizeof(array)/sizeof(array[0]); // size of array

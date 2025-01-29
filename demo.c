a44b29af8bb380d22a01e681749294a3d358951c
phzh <p.zhavoronkov@gmail.com>
2025-01-29 22:52:38 +0100

    array of structs

--- demo.c
+++ demo.c
@@ -1,26 +1,24 @@
 #include <stdio.h>
 #include <string.h>
 
-typedef struct {
-    char name[25];
-    char password[12];
-    int id;
-} User;
+struct Student {
+    char name[12];
+    float gpa;
+};
 
 int main() {
 
-    // typedef = reserved keyword that gives an existing datatype a "nickname"
+    struct Student student1 = {"Frank", 3.0};
+    struct Student student2 = {"John", 4.5};
+    struct Student student3 = {"Billy", 2.2};
+    struct Student student4 = {"Zeberdy", 5.1};
 
-    User user1 = {"Frank", "332211", 998877};
-    User user2 = {"John", "445566", 667788};
+    struct Student students[] = {student1, student2, student3, student4};
 
-    printf("%s\n", user1.name);
-    printf("%s\n", user1.password);
-    printf("%d\n", user1.id);
-    printf("\n");
-    printf("%s\n", user2.name);
-    printf("%s\n", user2.password);
-    printf("%d\n", user2.id);
+    for (int i = 0; i < sizeof(students)/sizeof(students[0]); i++) {
+        printf("%-12s", students[i].name); // -12 make indent / formatting to right
+        printf("%.2f\n", students[i].gpa);
+    }
 
     return 0;
 }

6776a99137649f833acc616b8725f6f39d7d4879
phzh <p.zhavoronkov@gmail.com>
2025-02-02 20:52:02 +0100

    guessing game with random numbers

--- demo.c
+++ demo.c
@@ -4,17 +4,35 @@
 
 int main() {
 
-    // pseudo random numbers = A set of values or elements that are statistically random
-    //                         (Don't use these for any sort of cryptographic security)
+    const int MIN = 1;
+    const int MAX = 100;
+    int guess;
+    int guesses = 0;
+    int answer;
 
+    // uses the current time as a seed to generate random numbers
     srand(time(0));
 
-    int number1 = (rand() % 20) + 1; // random number between 0 - 32 767
-    int number2 = (rand() % 20) + 1;
-    int number3 = (rand() % 20) + 1;
+    // generate a random number between MIN & MAX
+    answer = (rand() % MAX) + MIN; // % as a modulus operator
 
-    printf("%d\n", number1);
-    printf("%d\n", number2);
-    printf("%d\n", number3);
+    do {
+        printf("Enter a number between 1 and 100: ");
+        scanf("%d", &guess);
+        if (guess < answer) {
+            printf("The number is too small.\n");
+        } else if (guess > answer) {
+            printf("The number is too large.\n");
+        } else {
+            printf("The number is correct\n");
+        }
+        guesses++;
+    } while (guess != answer);
+
+    printf("*****************************\n");
+    printf("The answer: %d\n", answer);
+    printf("guesses: %d\n", guesses);
+    printf("*****************************");
+
     return 0;
 }

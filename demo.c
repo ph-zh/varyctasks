eaa5c3d087887343ece307e6178686e88ec7f710
phzh <p.zhavoronkov@gmail.com>
2025-02-17 22:05:02 +0100

    pointers

--- demo.c
+++ demo.c
@@ -1,64 +1,36 @@
 #include <stdio.h>
 
+void printAge(int pAge) {
+    printf("You are %d age old\n", pAge); // dereference
+}
 int main() {
 
-    // memory = an array of bytes within RAM (street)
-    // memory block = a single unit (byte) within memory, used to hold some value (person)
-    // memory address = the address of where a memory block is located (house address)
+    // pointer = a "variable-like" reference that holds a memory address to another variable, array, etc.
+    //           some tasks are performed more easily with pointers
+    //           * = indirection operator (value to address)
 
-    // decimal: 0-9 (1,2,3,4,5,6,7,8,9)
-    // hexadecimal: 0-9 + A - F (0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F)
+    // advantages of using pointers
+    // - less time in program execution.
+    // - working on the original variable.
+    // - with the help of pointers, we can create data structures (linked-list, stack, queue).
+    // - returning more than one values from functions.
+    // - searching and sorting large data very easy.
+    // - dynamically memory allocation.
 
-    char a = 'X';
-    char b = 'Y';
-    char c = 'Z';
+    int age = 21;
+    int *pAge = NULL; // good practice to assign NULL if declaring a pointer
+    pAge = &age;
 
-    short d = 'A';
-    short e = 'B';
-    short f = 'C';
+    // printf("address of age: %p\n", &age);
+    // printf("value of pAge: %p\n", pAge);
+    //
+    // printf("size of age: %d bytes\n", sizeof(age));
+    // printf("size of pAge: %d bytes\n", sizeof(pAge));
+    //
+    // printf("value of age: %d\n", age);
+    // printf("value at stored address: %d\n", *pAge);
 
-    int h = 'N';
-    int i = 'O';
-    int j = 'P';
-
-    char x;
-    double y[3];
-
-    printf("%d bytes\n", sizeof(a));
-    printf("%d bytes\n", sizeof(b));
-    printf("%d bytes\n", sizeof(c));
-
-    printf("%p\n", &a); // p - is format specifier to see the address of variable
-    printf("%p\n", &b);
-    printf("%p\n", &c);
-
-    printf("\n");
-
-    printf("%d short\n", sizeof(d));
-    printf("%d short\n", sizeof(e));
-    printf("%d short\n", sizeof(f));
-
-    printf("%p\n", &d);
-    printf("%p\n", &e);
-    printf("%p\n", &f);
-
-    printf("\n");
-
-    printf("%d int\n", sizeof(h));
-    printf("%d int\n", sizeof(i));
-    printf("%d int\n", sizeof(j));
-
-    printf("%p\n", &h);
-    printf("%p\n", &i);
-    printf("%p\n", &j);
-
-    printf("\n");
-
-    printf("%d bytes\n", sizeof(x));
-    printf("%d bytes\n", sizeof(y));
-
-    printf("%p\n", &x);
-    printf("%p\n", &y);
+    printAge(*pAge);
 
     return 0;
 }

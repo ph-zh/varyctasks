db4f6e9abf2a3be8dd9a62870b32d74e7446b536
phzh <p.zhavoronkov@gmail.com>
2025-04-19 17:47:32 +0200

    if else task

--- demo.c
+++ demo.c
@@ -1,147 +1,42 @@
+/*
+Исправьте код программы-советчика. Если до зарплаты остаётся меньше 10 дней, сообщения останутся прежними, если же больше 10 — пускай печатается совет для предыдущей категории. 
+Например, если на счету 15000 рублей и при этом до зарплаты остаётся 17 дней, то напечатается не «Неплохо! Прикупите долларов и зайдите поужинать в классное место.», а «Окей, пора в Макдак!».
+*/
 #include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <time.h>
-
-char board[3][3];
-const char PLAYER = 'x';
-const char COMPUTER = 'o';
-
-void resetBoard();
-void printBoard();
-int checkFreeSpaces();
-void playerMove();
-void computerMove();
-char checkWinner();
-void printWinner(char);
+#include <string.h>
 
 int main() {
 
-    char response;
+    double moneyBeforeSalary = 15000.0; // Количество денег до зарплаты
+    int daysBeforeSalary = 14;
 
-    do {
-
-        char winner = ' ';
-        char response = ' ';
-        resetBoard();
-
-        while(winner == ' ' && checkFreeSpaces() != 0) {
-            printBoard();
-            playerMove();
-            winner = checkWinner();
-            if(winner != ' ' || checkFreeSpaces() == 0) {
-                break;
-            }
-    
-            computerMove();
-            winner = checkWinner();
-            if(winner != ' ' || checkFreeSpaces() == 0) {
-                break;
-            }
+    if (moneyBeforeSalary < 3000) {
+        printf("Сегодня лучше поесть дома. Экономьте, и вы дотянете до зарплаты!\n");
+    } else if (moneyBeforeSalary < 10000) {
+        // Здесь нужное условие уже добавили
+        if (daysBeforeSalary < 10) {
+            printf("Окей, пора в Макдак!\n");
+        } else {
+            printf("Сегодня лучше поесть дома. Экономьте, и вы дотянете до зарплаты!\n");
         }
-    
-        printBoard();
-        printWinner(winner);
+        
+    } else if (moneyBeforeSalary < 30000) {
+        // Допишите условие
+        if (daysBeforeSalary < 10) {
+            printf("Неплохо! Прикупите долларов и зайдите поужинать в классное место.\n");
+        } else {
+            printf("Окей, пора в Макдак!\n");
+        }
 
-        printf("Whould you like to play again? (Y/N): \n");
-        scanf(" %c", &response);
-        response = toupper(response);
-    } while (response == 'Y');
-    
-    printf("Thanks for playing!\n");
+    } else {
+        // Если до зарплаты меньше 10 дней, то едим крабов
+        // Иначе — доллары и ужин в хорошем ресторане
+        if (daysBeforeSalary < 10) {
+            printf("Класс! Заказывайте крабов!\n");
+        } else {
+            printf("Неплохо! Прикупите долларов и зайдите поужинать в классное место.\n");
+        }       
+    }
 
     return 0;
-}
-
-void resetBoard() {
-    for(int i = 0; i < 3; i++) {
-        for(int j = 0; j < 3; j++) {
-            board[i][j] = ' ';
-        }
-    }
-}
-void printBoard() {
-    printf(" %c | %c | %c", board[0][0], board[0][1], board[0][2]);
-    printf("\n---|---|---\n");
-    printf(" %c | %c | %c", board[1][0], board[1][1], board[1][2]);
-    printf("\n---|---|---\n");
-    printf(" %c | %c | %c", board[2][0], board[2][1], board[2][2]);
-    printf("\n");
-}
-int checkFreeSpaces() {
-    int freeSpaces = 9;
-    for(int i = 0; i < 3; i++) {
-        for(int j=0; j < 3; j++) {
-            if(board[i][j] != ' ') {
-                freeSpaces--;
-            }
-        }
-    }
-    return freeSpaces;
-}
-void playerMove() {
-    int x = 0;
-    int y = 0;
-
-    do {
-        printf("Enter row #(1-3): ");
-        scanf("%d", &x);
-        x--;
-        printf("Enter column #(1-3): ");
-        scanf("%d", &y);
-        y--;
-    
-        if(board[x][y] != ' ') {
-            printf("Invalid move!\n");
-        } else {
-            board[x][y] = PLAYER;
-            break;
-        }
-    } while (board[x][y] !=' ');
-}
-void computerMove() {
-    // creates a seed based on curent time
-    srand(time(0));
-    int x = 0;
-    int y = 0;
-    if(checkFreeSpaces() > 0) {
-        do {
-            x = rand() % 3;
-            y = rand() % 3;
-        } while (board[x][y] != ' ');
-        board[x][y] = COMPUTER;
-    } else {
-        printWinner(' ');
-    }
-}
-char checkWinner(){
-    //check rows
-    for (int i = 0; i < 3; i++) {
-        if(board[i][0] == board[i][1] && board[i][0] == board[i][2]) {
-            return board[i][0];
-        }
-    }
-    //check columns
-    for (int i = 0; i < 3; i++) {
-        if(board[0][i] == board[1][i] && board[0][i] == board[2][i]) {
-            return board[0][i];
-        }
-    }
-    //check diagolans
-    if(board[0][0] == board[1][1] && board[0][0] == board[2][2]) {
-        return board[0][0];
-    }
-    if(board[0][2] == board[1][1] && board[0][2] == board[2][0]) {
-        return board[0][2];
-    }
-    return ' ';
-}
-void printWinner(char winner){
-    if(winner == PLAYER) {
-        printf("You Win!\n");
-    } else if(winner == COMPUTER) {
-        printf("You lose!\n");
-    } else {
-        printf("Its a tie!\n");
-    }
 }

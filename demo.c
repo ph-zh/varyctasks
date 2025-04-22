79410cfc35b9aa00894feb55065a3a87084642b3
phzh <p.zhavoronkov@gmail.com>
2025-04-22 23:08:33 +0200

    converted loop task form java

--- demo.c
+++ demo.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    int days = 5;
-    int moneyPerDay = 200;
-    int sum = 0;
+    int money = 500; // Деньги на корм
+    int foodPerDay = 100; // Стоимость пачки корма
+    int days = 0; // Дни, когда вы сможете покупать корм
 
-    for(int i = 1; i <= days; i++) {
-        sum = sum + moneyPerDay;
-        printf("Day %d already %d dollars\n", i, sum);
+    for(int i = money - foodPerDay; i >= 0; i = i - foodPerDay) {
+        days = days + 3;
+        printf("On %d day you will have %d Dollars\n", days, i);
     }
+    printf("Your money will be enough for %d days\n", days);
 
     return 0;
 }

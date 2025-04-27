e159478110bff0a1ea3d7a1e63078c82671cc7eb
phzh <p.zhavoronkov@gmail.com>
2025-04-27 20:07:36 +0200

    random function introduction

--- demo.c
+++ demo.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main() {
-    int flatsNumber = 5; // Количество квартир на этаже
-    int floorsNumber = 10; // Количество этажей
 
-    for (int i = 1; i <= floorsNumber; i++) {
-        for (int j = 1; j <= flatsNumber; j++) {
-            int currentFlat = (i - 1) * flatsNumber + j; // Заготовка формулы для учёта квартир
-            printf("Этаж %d квартира %d — доставлено.\n", i, currentFlat);
-        }
+    int dayCount = 0; // Для учёта дней накоплений
+    int moneyTotal = 0; // Суммарное количество накоплений
+    int moneyToday; // Сколько откладываем сегодня
+    int goal = 5000; // Финансовая цель
+
+    srand(time(NULL));
+
+    while (moneyTotal <= goal) {
+        moneyToday = rand()%300; // Случайная сумма на сегодня
+        moneyTotal = moneyTotal + moneyToday; // Добавили эти деньги в копилку
+        dayCount = dayCount + 1; // Засчитали день
     }
 
+    printf("Ура! Вы смогли накопить %d за %d дней.\n", goal, dayCount);
     return 0;
 }

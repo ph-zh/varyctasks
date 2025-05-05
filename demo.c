6d9091aeaa7f79e500fe4f9ed7e17e1a13b6c8fd
phzh <p.zhavoronkov@gmail.com>
2025-05-05 23:56:58 +0200

    update

--- demo.c
+++ demo.c
@@ -7,7 +7,7 @@ int main() {
     int randomInt = rand()%1001;
     int userInput = -1;
 
-    printf("Я загадал число от 0 до 1000.\n");
+    printf("Я загадал число от 0 до 1001.\n");
     printf("Ваш ход:\n");
     
     while(randomInt != userInput) {

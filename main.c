ceb279a48b192e99fbe695b59f8a80a868c1fbaa
phzh <p.zhavoronkov@gmail.com>
2026-05-27 11:59:59 +0200

    variable

--- main.c
+++ main.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 
 int main() {
-    // comment #1
 
-    /*
-     comment
-     #
-     2
-     */
+    int age = 25;
+    int year = 2025;
+    int quantity = 2;
+    float gpa = 2.5;
+    float price = 19.99;
+    float temperature = -10.1;
 
-    printf("test message 1\n");
-    printf("test message 2");
+    printf("You are %d years old\n", age);
+    printf("the year is %d\n", year);
+    printf("You have ordered %d x items\n", quantity);
+    printf("Your gpa is %.1f\n", gpa);
+    printf("the price is %.2f\n", price);
+    printf("the temperature is %f F\n", temperature);
+
     return 0;
 }
